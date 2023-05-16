884596fae9362d2dc7bbf6bd44a606692fea0c25
mattfarmilo <longrnr56@gmail.com>
2023-05-16 21:29:31 +1000

    Create grid layout
    Create grid of empty divs where the size of the divs is determined
    by the number of divs that need to fit into the screen.  Attempted to
    add event listener to change colour of divs on hover, but not yet
    working
    
    [Ticket: X]

--- script.js
+++ script.js
@@ -3,16 +3,28 @@ const container = document.querySelector('.container');
 
 //Function to create required number of divs
 function createDivs (num) {
-  for (let i = size * size; i > 0; i--) {
+  for (let i = num * num; i > 0; i--) {
     const div = document.createElement('div');
     div.classList.add('child')
     container.appendChild(div);
   }
 }
 
-//Function to format divs into grid
+//Function to format divs into grid and add event listeners
+function formatDivs (num) {
+  const containerSize = container.clientHeight;
+  const childSize = Math.floor(containerSize / num);
+  const children = document.querySelectorAll('.child')
 
+  children.forEach((child) => {
+    child.setAttribute('style', `height: ${childSize}px; width: ${childSize}px;`)
+    child.addEventListener('hover', () => {
+      child.setAttribute('style', 'background-color: black;');
+    })
+  })
+}
 
 //Website
 const size = 2;
 createDivs(size);
+formatDivs(size);
