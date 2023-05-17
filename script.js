8395b2ec70d3ba7d894b4688304866436881a12d
mattfarmilo <longrnr56@gmail.com>
2023-05-17 22:28:23 +1000

    Add standard, rainbow and shading modes
    centered output and added a selector for standard, rainbow and shading
    modes.  Added both standard and rainbow event listeners.  Shading mode
    not yet operational.

--- script.js
+++ script.js
@@ -1,30 +1,63 @@
-//Create container as global variable
-const container = document.querySelector('.container');
-
 //Function to create required number of divs
 function createDivs (num) {
+  const container = document.querySelector('.container');
+  const childSize = 650 / num;
+  
   for (let i = num * num; i > 0; i--) {
     const div = document.createElement('div');
     div.classList.add('child')
+    div.setAttribute('style', `height: ${childSize}px; width: ${childSize}px; background-color: rgb(255,255,255);`);
     container.appendChild(div);
   }
+
+  hoverEvent();
 }
 
-//Function to format divs into grid and add event listeners
-function formatDivs (num) {
-  const containerSize = container.clientHeight;
-  const childSize = Math.floor(containerSize / num);
-  const children = document.querySelectorAll('.child')
+//Function to add event listener to divs
+ function hoverEvent () {
+  const divs = document.querySelectorAll('.child');
+  const mode = document.querySelector('#mode').value;
 
-  children.forEach((child) => {
-    child.setAttribute('style', `height: ${childSize}px; width: ${childSize}px;`)
-    child.addEventListener('hover', () => {
-      child.setAttribute('style', 'background-color: black;');
-    })
-  })
-}
+  divs.forEach(div => {
+    div.addEventListener('mouseover', (e) => {
+      if (mode === 'standard') {
+        e.target.style.backgroundColor = 'black';
+      }
+      if (mode === 'rainbow') {
+        e.target.style.backgroundColor = `rgb(${randNum()}, ${randNum()}, ${randNum()})`;
+      }
+      if (mode === 'shade') {
+        console.log(e.target.style.backgroundColor);
+      }
+    });
+  });
+ }
+
+ //Function to generate random number
+ function randNum() {
+  return Math.floor(Math.random() * 256);
+ }
+
+ //Function to reset page and set dimensions
+ function reset () {
+  const container = document.querySelector('.container');
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+
+  size = Number(prompt('How many pixels on each side of your grid?'));
+
+  if (size > 100 || size < 1) {
+    alert('Please enter a number from 1-100');
+    reset();
+  }
+  createDivs(size);
+ }
 
 //Website
-const size = 2;
+let size = 16;
 createDivs(size);
-formatDivs(size);
+
+//Add event listener to button
+const btn = document.querySelector('button');
+btn.addEventListener('click', reset);
